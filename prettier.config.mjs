f204a0f4ab4b924bfe0553ac95f658a8c363bce0
Sanskar <sanskarin@outlook.in>
2026-08-21 06:44:08 +0530

    chore: configure code formatting

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+const config = {
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  tabWidth: 2,
+};
+
+export default config;
