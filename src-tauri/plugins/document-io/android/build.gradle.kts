1e63d4bd177f36dc9e744cd0dd59e89a5c250eea
Sanskar <sanskarin@outlook.in>
2026-08-24 13:35:21 +0530

    build: configure Android document IO library

---
+++ src-tauri/plugins/document-io/android/build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "in.sanskar.markora.documentio"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 24
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_1_8
+    }
+}
+
+dependencies {
+    implementation(project(":tauri-android"))
+}
